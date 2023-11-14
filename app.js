3bed837c0600294e829be0faee3905f7819b0628
BoximItInderstrit <150811144+BoximItInderstrit@users.noreply.github.com>
2023-11-14 21:45:07 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -13,13 +13,16 @@ let btn4 = document.getElementById("btn4");
 let btn5 = document.getElementById("btn5");
 let btn6 = document.getElementById("btn6");
 let btn7 = document.getElementById("btn7");
+let btn8 = document.getElementById("btn8");
+let btn9 = document.getElementById("btn9");
+let btn10 = document.getElementById("btn10");
 
 btn1.addEventListener("click", function(){
 	if (tg.MainButton.isVisible) {
 		tg.MainButton.hide();
 	}
 	else {
-		tg.MainButton.setText("Вы выбрали товар 1!");
+		tg.MainButton.setText("Sdelat Tatu 1!");
 		item = "1";
 		tg.MainButton.show();
 	}
@@ -30,7 +33,7 @@ btn2.addEventListener("click", function(){
 		tg.MainButton.hide();
 	}
 	else {
-		tg.MainButton.setText("Вы выбрали товар 2!");
+		tg.MainButton.setText("Sdelat Tatu 2!");
 		item = "2";
 		tg.MainButton.show();
 	}
@@ -41,7 +44,7 @@ btn3.addEventListener("click", function(){
 		tg.MainButton.hide();
 	}
 	else {
-		tg.MainButton.setText("Вы выбрали товар 3!");
+		tg.MainButton.setText("Sdelat Tatu 3!");
 		item = "3";
 		tg.MainButton.show();
 	}
@@ -52,7 +55,7 @@ btn4.addEventListener("click", function(){
 		tg.MainButton.hide();
 	}
 	else {
-		tg.MainButton.setText("Вы выбрали товар 4!");
+		tg.MainButton.setText("Sdelat Tatu 4!");
 		item = "4";
 		tg.MainButton.show();
 	}
@@ -63,7 +66,7 @@ btn5.addEventListener("click", function(){
 		tg.MainButton.hide();
 	}
 	else {
-		tg.MainButton.setText("Вы выбрали товар 5!");
+		tg.MainButton.setText("Sdelat Tatu 5!");
 		item = "5";
 		tg.MainButton.show();
 	}
@@ -74,7 +77,7 @@ btn6.addEventListener("click", function(){
 		tg.MainButton.hide();
 	}
 	else {
-		tg.MainButton.setText("Вы выбрали товар 6!");
+		tg.MainButton.setText("Sdelat Tatu 6!");
 		item = "6";
 		tg.MainButton.show();
 	}
@@ -85,12 +88,44 @@ btn7.addEventListener("click", function(){
 		tg.MainButton.hide();
 	}
 	else {
-		tg.MainButton.setText("Вы выбрали товар 7!");
+		tg.MainButton.setText("Sdelat Tatu 7!");
 		item = "7";
 		tg.MainButton.show();
 	}
 });
 
+btn8.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Sdelat Tatu 8!");
+		item = "8";
+		tg.MainButton.show();
+	}
+});
+
+btn7.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Sdelat Tatu 9!");
+		item = "9";
+		tg.MainButton.show();
+	}
+});
+
+btn7.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Sdelat Tatu 10!");
+		item = "10";
+		tg.MainButton.show();
+	}
+});
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
 	tg.sendData(item);
