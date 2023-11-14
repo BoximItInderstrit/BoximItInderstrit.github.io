5c687856f2ebc7f6bfe6dbd48aa4b1a7f10961bb
BoximItInderstrit <150811144+BoximItInderstrit@users.noreply.github.com>
2023-11-14 15:44:23 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -7,21 +7,17 @@ tg.MainButton.color = '#2cab37';
 
 let item = "";
 
-// Создаем массив кнопок
-let buttons = [];
-
+// Добавлены обработчики для новых кнопок
 for (let i = 1; i <= 12; i++) {
-    buttons.push(document.getElementById(`btn${i}`));
-}
-
-// Добавляем слушатель события на каждую кнопку
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", function(){
+    let btn = document.getElementById(`btn${i}`);
+    
+    btn.addEventListener("click", function(){
         if (tg.MainButton.isVisible) {
             tg.MainButton.hide();
-        } else {
-            tg.MainButton.setText(`Вы выбрали товар ${i + 1}!`);
-            item = String(i + 1);
+        }
+        else {
+            tg.MainButton.setText(`Вы выбрали товар ${i}!`);
+            item = `${i}`;
             tg.MainButton.show();
         }
     });
@@ -32,7 +28,9 @@ Telegram.WebApp.onEvent("mainButtonClicked", function(){
 });
 
 let usercard = document.getElementById("usercard");
+
 let p = document.createElement("p");
 
 p.innerText = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
+
 usercard.appendChild(p);
