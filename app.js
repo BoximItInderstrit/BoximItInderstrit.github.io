649ae11ebf1353361881f27ae998045636981bc4
BoximItInderstrit <150811144+BoximItInderstrit@users.noreply.github.com>
2023-11-14 23:30:47 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ tg.expand();
 
 tg.MainButton.textColor = '#FFFFFF';
 tg.MainButton.color = '#2cab37';
+
 let item = "";
 
 let btn1 = document.getElementById("btn1");
@@ -17,126 +18,64 @@ let btn8 = document.getElementById("btn8");
 let btn9 = document.getElementById("btn9");
 let btn10 = document.getElementById("btn10");
 
-btn1.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Sdelat Tatu 1!");
-		item = "1";
-		tg.MainButton.show();
-	}
+btn1.addEventListener("click", function () {
+    updateButton("Sdelat Tatu 1!", "1");
 });
 
-btn2.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Sdelat Tatu 2!");
-		item = "2";
-		tg.MainButton.show();
-	}
+btn2.addEventListener("click", function () {
+    updateButton("Sdelat Tatu 2!", "2");
 });
 
-btn3.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Sdelat Tatu 3!");
-		item = "3";
-		tg.MainButton.show();
-	}
+btn3.addEventListener("click", function () {
+    updateButton("Sdelat Tatu 3!", "3");
 });
 
-btn4.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Sdelat Tatu 4!");
-		item = "4";
-		tg.MainButton.show();
-	}
+btn4.addEventListener("click", function () {
+    updateButton("Sdelat Tatu 4!", "4");
 });
 
-btn5.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Sdelat Tatu 5!");
-		item = "5";
-		tg.MainButton.show();
-	}
+btn5.addEventListener("click", function () {
+    updateButton("Sdelat Tatu 5!", "5");
 });
 
-btn6.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Sdelat Tatu 6!");
-		item = "6";
-		tg.MainButton.show();
-	}
+btn6.addEventListener("click", function () {
+    updateButton("Sdelat Tatu 6!", "6");
 });
 
-btn7.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Sdelat Tatu 7!");
-		item = "7";
-		tg.MainButton.show();
-	}
+btn7.addEventListener("click", function () {
+    updateButton("Sdelat Tatu 7!", "7");
 });
 
-btn8.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Sdelat Tatu 8!");
-		item = "8";
-		tg.MainButton.show();
-	}
+btn8.addEventListener("click", function () {
+    updateButton("Sdelat Tatu 8!", "8");
 });
 
-btn7.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Sdelat Tatu 9!");
-		item = "9";
-		tg.MainButton.show();
-	}
+btn9.addEventListener("click", function () {
+    updateButton("Sdelat Tatu 9!", "9");
 });
 
-btn7.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Sdelat Tatu 10!");
-		item = "10";
-		tg.MainButton.show();
-	}
+btn10.addEventListener("click", function () {
+    updateButton("Sdelat Tatu 10!", "10");
 });
 
-Telegram.WebApp.onEvent("mainButtonClicked", function(){
-	tg.sendData(item);
-});
+function updateButton(text, itemId) {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    } else {
+        tg.MainButton.setText(text);
+        item = itemId;
+        tg.MainButton.show();
+    }
+}
 
+Telegram.WebApp.onEvent("mainButtonClicked", function () {
+    tg.sendData(item);
+});
 
 let usercard = document.getElementById("usercard");
 
 let p = document.createElement("p");
 
-p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}`;
+p.innerText = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
 
 usercard.appendChild(p);
